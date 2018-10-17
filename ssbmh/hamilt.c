6740a27bea9172912c0e9cd27a6209fa9dd48b64
Thomas Leberbauer <slosd@posteo.net>
2018-10-17 22:39:38 +0200

    Reading the graph

--- ssbmh/hamilt.c
+++ ssbmh/hamilt.c
@@ -1,53 +1,79 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 typedef struct
 {
   int v1,v2;
   int c;
 } edge;
-int* traverse(edge* graph, int sn)
+
+typedef struct
 {
-  int v = graph[0].v1;
-  int* path;
-  path = (int*) malloc ((sn)*sizeof(int));
+  int n, m;
+  edge * edges;
+} graph;
+
+int * traverse (graph * g)
+{
+  int * path = (int*) malloc ((g->n) * sizeof(int));
+  int v = g->edges[0].v1;
   path[0] = v;
-  for(int i=1; i<= sn; i++)
+  for(int i = 1; i <= g->n; i++)
     {
-      int nextn = graph[v-1].v2;
+      int nextn = g->edges[v-1].v2;
       path[i] = nextn;
       v = nextn;
     }
   return path;
 }
 
-int main (int argc, char** argv)
+graph * new_graph (int n, int m)
 {
-  int * nodes;
-  edge * graph;
-  int sn;
-  int se;
-  FILE * fs;
-  fs = fopen(argv[1],"r");
-  printf("%s\n", argv[0]);
-  fscanf(fs, "%d %d", &sn, &se);
-  int i = 0;
-  graph = (edge*) malloc ((se)*sizeof(edge));
-  while (fscanf(fs, "%d %d %d\n", &(graph[i].v1),
-		&(graph[i].v2), &(graph[i].c)) != -1)
-    {
-      printf("%d -- %d\n", graph[i].v1,graph[i].v2);
-      i+=1;
-    }
-  int * path = traverse(graph, sn);
-  for (i=0;i<=sn;i++)
-    {
-      printf("%d ",path[i]);
-    }
-  printf("\n");
-  free(graph);
-  free(path);
-  printf("%d %d\n", sn, se);
-  fclose(fs);
-  return 0;
+  graph * g = (graph*) malloc(sizeof(graph));
+  g->n = n;
+  g->m = m;
+  g->edges = (edge*) malloc(m * sizeof(edge));
+  return g;
 }
 
+void free_graph (graph * g)
+{
+  free(g->edges);
+  free(g);
+}
+
+graph * graph_from_file (char* file)
+{
+  int n, m;
+  FILE * fs;
+  fs = fopen(file, "r");
+  fscanf(fs, "%d %d", &n, &m);
+  graph * g = new_graph(n, m);
+  int i = 0;
+  while (fscanf(fs, "%d %d %d\n",
+	  &(g->edges[i].v1), &(g->edges[i].v2), &(g->edges[i].c)) != -1)
+    {
+      i += 1;
+    }
+  fclose(fs);
+  return g;
+}
+
+int main (int argc, char** argv)
+{
+  graph * g = graph_from_file(argv[1]);
+  for (int i = 0; i < g->m; i++)
+    {
+      printf("%d -- %d\n", g->edges[i].v1, g->edges[i].v2);
+    }
+
+  int * path = traverse(g);
+  for (int i = 0; i <= g->n; i++)
+    {
+      printf("%d ", path[i]);
+    }
+  printf("\n");
+  free_graph(g);
+  free(path);
+  return 0;
+}
