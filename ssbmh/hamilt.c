00e92041c11cbf2692b77eb9c528469b1de4e7db
dcguim <daniel.coelhoguimaraes@stud-inf.unibz.it>
2018-10-29 12:01:28 +0100

    Refactor the code to treat edges as pair of vertices; took the absolute value of the objective function, and freed the edges graph struct

--- ssbmh/hamilt.c
+++ ssbmh/hamilt.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct edge
-{
-  int v1, v2;
-  int c;
-} edge;
-
+struct edge;
 typedef struct vertex
 {
+  int id;
   int degree;
-  edge * edges;
+  struct edge * edges;
 } vertex;
+
+typedef struct edge
+{
+  vertex v1, v2;
+  int c;
+} edge;
 
 typedef struct graph
 {
@@ -31,15 +33,16 @@ void free_path (path * p)
   free(p->path);
 }
 
+
 path traverse (graph * g)
 {
   path p;
   p.path = (int*) malloc ((g->n) * sizeof(int));
-  int v = g->edges[0].v1;
+  int v = g->edges[0].v1.id;
   p.path[0] = v;
   for (int i = 1; i <= g->n; i++)
     {
-      int nextn = g->edges[v].v2;
+      int nextn = g->edges[v].v2.id;
       p.path[i] = nextn;
       v = nextn;
     }
@@ -53,7 +56,7 @@ edge * find_edge_from_to (graph * g, int a, int b)
   int d = v.degree;
   for (int i = 0; i < d; ++i)
     {
-      if (v.edges[i].v2 == b)
+      if (v.edges[i].v2.id == b)
 	{
 	  return v.edges + i;
 	}
@@ -72,8 +75,7 @@ graph * new_graph (int n, int m)
 }
 
 void free_graph (graph * g)
-{
-  free(g->edges);
+{  
   for (int i = 0; i < g->n; ++i)
     {
       free(g->vertices[i].edges);
@@ -88,7 +90,7 @@ void graph_read_edges (graph * g, FILE * fs)
   while (k != -1)
     {
       k = fscanf(fs, "%d %d %d\n",
-	    &(g->edges[i].v1), &(g->edges[i].v2), &(g->edges[i].c));
+	    &(g->edges[i].v1.id), &(g->edges[i].v2.id), &(g->edges[i].c));
       i += 1;
     }
   //TODO calculate M
@@ -99,17 +101,23 @@ void graph_init_vertices (graph * g)
 {
   int n = g->n, m = g->m;
   int * degrees = (int*) malloc(n * sizeof(int));
+  // Inititalize the incident edges of every vertex to zero
   for (int i = 0; i < n; ++i)
     {
       degrees[i] = 0;
     }
-
+  // Set the number of incident edges of every vertex to the degree variable
   for (int i = 0; i < m; ++i)
     {
-      degrees[g->edges[i].v1] += 1;
-      degrees[g->edges[i].v2] += 1;
+      degrees[g->edges[i].v1.id] += 1;
+      degrees[g->edges[i].v2.id] += 1;
     }
 
+  for (int i = 0; i < n; ++i)
+    {
+      printf("deg [%d]: %d\n", i,degrees[i]);
+    }
+  // Create the adj list on the vertices
   vertex * v;
   for (int i = 0; i < n; ++i)
     {
@@ -122,15 +130,16 @@ void graph_init_vertices (graph * g)
     {
       e = g->edges[i];
 
-      v = g->vertices + e.v1;
-      v->edges[--degrees[e.v1]] = e;
+      v = g->vertices + e.v1.id;
+      v->edges[--degrees[e.v1.id]] = e;
 
-      v = g->vertices + e.v2;
-      t.v1 = e.v2;
-      t.v2 = e.v1;
+      v = g->vertices + e.v2.id;
+      t.v1.id = e.v2.id;
+      t.v2.id = e.v1.id;
       t.c = e.c;
-      v->edges[--degrees[e.v2]] = t;
+      v->edges[--degrees[e.v2.id]] = t;
     }
+  free(g->edges);
   free(degrees);
 }
 
@@ -139,11 +148,20 @@ graph * graph_from_file (char * file)
   int n, m;
   FILE * fs;
   fs = fopen(file, "r");
+  int i;
+  
+  printf("Enter graph from file\n");
+  fflush(stdout);
   fscanf(fs, "%d %d", &n, &m);
   graph * g = new_graph(n, m);
-  graph_read_edges (g, fs);
+  graph_read_edges (g, fs);  
   fclose(fs);
-  graph_init_vertices (g);
+  printf("m:%d, n:%d\nedges:\n",g->m, g->n);
+  for (i=0; i<g->m;i++)
+    {
+      printf("(%d %d) c:%d\n", g->edges[i].v1.id, g->edges[i].v2.id,g->edges[i].c);
+    }
+  graph_init_vertices(g);
   return g;
 }
 
@@ -158,13 +176,13 @@ int cbtsp_o(graph * g, path * p)
       e = find_edge_from_to (g, p->path[i], p->path[i + 1]);
       o += (e == NULL) ? g->bigM : e->c;
     }
-  return o;
+  return abs(o);
 }
 
 int main (int argc, char** argv)
 {
-  graph * g = graph_from_file(argv[1]);
-  path p = traverse(g);
+  graph * g = graph_from_file(argv[1]);  
+  path p = traverse(g);  
   printf("path: ");
   for (int i = 0; i < p.length; ++i)
     {
