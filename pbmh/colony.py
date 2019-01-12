562e3e7ce9b73bb4135e8ef1b421431d099f0c7b
Thomas Leberbauer <slosd@posteo.net>
2019-01-12 16:48:15 +0100

    Make Colony easier to extend

--- pbmh/colony.py
+++ pbmh/colony.py
@@ -19,56 +19,56 @@ class Colony():  # required params
         self.evaprate = evap_rate
         self.alp=alph
         self.bet=beta
-        self.__g = graph
-        self.__pheromone = {}
+        self._g = graph
+        self._pheromone = {}
         self.l = Lock()
         self.initPheromoneModel()
         self.init = False
         self.runColony()
 
     def initPheromoneModel(self):
-        for e in self.__g.edges:
-            self.__pheromone[e] = 0.5
+        for e in self._g.edges:
+            self._pheromone[e] = 0.5
         print('pherormone model')
-        print(self.__pheromone)
+        print(self._pheromone)
         
     def pickRandomNode(self):
-        return choice(list(self.__g.nodes))
+        return choice(list(self._g.nodes))
     
     def getPhero(self,i,j):
-        if (i,j) in self.__pheromone.keys():
-            return self.__pheromone[(i,j)]        
-        elif (j,i) in self.__pheromone.keys():
-            return self.__pheromone[(j,i)]
+        if (i,j) in self._pheromone.keys():
+            return self._pheromone[(i,j)]
+        elif (j,i) in self._pheromone.keys():
+            return self._pheromone[(j,i)]
         else:
             print("Unexisting ({0},{1}) pheromone/edge".format(
                 str(i),str(j)))
             return 0
         
     def setPhero(self,i,j,v):
-        if (i,j) in self.__pheromone.keys():
-            self.__pheromone[(i,j)] = v       
-        elif (j,i) in self.__pheromone.keys():
-            self.__pheromone[(j,i)] = v
+        if (i,j) in self._pheromone.keys():
+            self._pheromone[(i,j)] = v
+        elif (j,i) in self._pheromone.keys():
+            self._pheromone[(j,i)] = v
         else:
             print("Unexisting ({0},{1}) pheromone/edge".format(
                 str(i),str(j)))
-            self.__pheromone[(i,j)] = 0
+            self._pheromone[(i,j)] = 0
 
     def univisited(self, init):
-        return [n for n in self.__g.nodes if n != init]
+        return [n for n in self._g.nodes if n != init]
         
     def getWeight(self,i,j):        
-        if  (i,j) in self.__g.edges:
-            return self.__g[i][j]['weight']
-        elif (j,i) in self.__g.edges:
-            return self.__g[j][i]['weight']
+        if  (i,j) in self._g.edges:
+            return self._g[i][j]['weight']
+        elif (j,i) in self._g.edges:
+            return self._g[j][i]['weight']
         else:
             raise ValueError("Unexisting ({0},{1}) weight/edge".format(
                 str(i),str(j)))
         
     def pheroEvaporate(self):
-        for e in self.__g.edges:
+        for e in self._g.edges:
             prev_phero = self.getPhero(e[0],e[1])
             next_phero = (1 - self.evaprate)*prev_phero
             self.setPhero(e[0],e[1],next_phero)
@@ -77,25 +77,27 @@ class Colony():  # required params
         print("run colony ...")
         base = 0
         for i in range(self.maxit):
-            base = self.size*i
-            for j in range(self.size):
-                # if thread base+j is alive wait for it to terminate,
-                # before spawning it again                
-                print("starting thread {0}".format(j))
-                self.ants.append(Ant(base+j+1,self.rinit,self))
-                self.ants[base+j].start()
-            self.init = True
-            for j in range(self.size):
-                if self.ants[base+j].is_alive():
-                    print("is {0} alive?".format(base+j))
-                    self.ants[base+j].join()
-            
-                
+            self.runIt(i)
         print('best path found so far: {0}; objfun: {1}'.format(
             self.bestpath,self.bestobj))
 
+    def runIt(self, i):
+        base = self.size*i
+        for j in range(self.size):
+            # if thread base+j is alive wait for it to terminate,
+            # before spawning it again
+            print("starting thread {0}".format(j))
+            self.ants.append(Ant(base+j+1,self.rinit,self))
+            self.ants[base+j].start()
+        self.init = True
+        for j in range(self.size):
+            if self.ants[base+j].is_alive():
+                print("is {0} alive?".format(base+j))
+                self.ants[base+j].join()
+
+
     def neighborhood(self,node,path):
-        return [n for n in self.__g[node].keys() if n not in path]
+        return [n for n in self._g[node].keys() if n not in path]
 
     def pheroUpdate(self, tid, path):
         "update the pheromone from thread tid path"
@@ -104,7 +106,7 @@ class Colony():  # required params
         print("thread "+str(tid)+" acquired lock for path:")
         print(path)
         self.pheroEvaporate()
-        o = obj(self.__g,path)
+        o = obj(self._g,path)
         if o < self.bestobj:
             self.bestobj = o
             self.bestpath = path
@@ -118,6 +120,6 @@ class Colony():  # required params
                     self.setPhero(path[i],path[i+1], prev_phero + 1/o)
             else:
                 self.setPhero(path[i],path[i+1], prev_phero + 1/o)
-        print(self.__pheromone)        
+        print(self._pheromone)
         self.l.release()
         print("thread "+str(tid)+" released lock" )
